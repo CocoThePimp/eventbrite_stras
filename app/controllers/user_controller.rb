d7dc0a96270f377396f24a1e2699c95a288680e9
Maxime Smolis <mpsmolis@gmail.com>
2019-02-12 18:42:37 +0100

    method : you can only see your own page added

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,19 @@
 class UserController < ApplicationController
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  # before_action :only_see_your_own_page
 
   def show
     @user = User.find(params[:id])
     @event = Event.where(user_id: @user)
   end
+
+
+  def only_see_your_own_page
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to root_path, notice: "AHAH Bien essayé ! mais non…"
+    end
+  end
+
 end
