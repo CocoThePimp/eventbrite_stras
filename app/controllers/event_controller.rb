92dfc6b96910f2f7fd44838b1baaf581f3f9ecd5
Maxime Smolis <mpsmolis@gmail.com>
2019-02-12 21:22:22 +0100

    event create okay

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -4,6 +4,23 @@ class EventController < ApplicationController
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create 
+    @event = Event.new(title: params[:title], 
+                        description: params[:description], 
+                        start_date: params[:start_date], 
+                        duration: params[:duration], 
+                        location: params[:location], 
+                        price: params[:price],
+                        user_id: current_user.id)
+    if @event.save
+      redirect_to '/'
+    else
+      puts @event.errors.full_messages
+      render 'new'
+    end
   end
 
   def show
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,7 @@
 class UserController < ApplicationController
 
-  # before_action :authenticate_user!
-  # before_action :only_see_your_own_page
+  before_action :authenticate_user!
+  before_action :only_see_your_own_page
 
   def show
     @user = User.find(params[:id])
