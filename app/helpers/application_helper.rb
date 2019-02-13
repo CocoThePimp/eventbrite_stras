02de0c85696f097665ee4888f35cca6ece8cba87
Maxime Smolis <mpsmolis@gmail.com>
2019-02-13 23:53:25 +0100

    bouton rejoindre event si co mais pas admin ou participant ajouté

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
 module ApplicationHelper
   include UserHelper
+  include EventHelper
 end
--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,2 +1,15 @@
 module EventHelper
+
+  def participate
+    if current_user != @event.user
+       Attendance.all.each do |attendance|
+         if attendance.user_id == current_user.id && attendance.event_id == @event.id
+           @i = false
+           break
+         else
+           @i = true
+         end
+       end
+     end
+   end
 end
